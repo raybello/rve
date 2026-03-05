a4cf1b3d89d08469b9e2239757ee53f07b95fbb8
RaymondBello <raymondbello11@gmail.com>
2026-03-05 01:56:44 -0500

    Added building linux in docker

--- docker/container.mk
+++ docker/container.mk
@@ -3,21 +3,28 @@ WORKDIR=/workspace/project
 OUTPUT=workspace/output
 
 JOBS=$(shell nproc)
+BR2_TAR_OPTIONS ?= --no-same-owner
 
-.PHONY: build toolchain buildroot linux
-build: toolchain buildroot linux
+.PHONY: build toolchain linux
+build: toolchain linux
 	cd $(WORKDIR)
 	echo "Building Linux for RISC-V with $(JOBS) parallel jobs..."
 
 toolchain:
 	cd $(WORKDIR)
 	echo "Setting up toolchain..."
-
-buildroot:
-	cd $(WORKDIR)
-	echo "Setting up Buildroot..."
+	cp -a configs/custom_kernel_config buildroot/kernel_config
+	cp -a configs/buildroot_config buildroot/.config
+	cp -a configs/busybox_config buildroot/busybox_config
+	cp -a configs/uclibc_config buildroot/uclibc_config
+	cp -a configs/uclibc_config buildroot/uclibc_config_extra
+	make -C buildroot BR2_TAR_OPTIONS="$(BR2_TAR_OPTIONS)" -j$(JOBS)
+	cp -a configs/rootfsoverlay/* buildroot/output/target/
 
 linux:
 	cd $(WORKDIR)
 	echo "Building Linux kernel..."
+	cp -a configs/rootfsoverlay/* buildroot/output/target/
+	make -C buildroot BR2_TAR_OPTIONS="$(BR2_TAR_OPTIONS)" -j$(JOBS)
+	cp -a buildroot/output/images/Image $(OUTPUT)/
 
